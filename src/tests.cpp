22ea65c454612ccc02661acd3b46780b842a4381
Danila-Code <danila.Zcode@gmail.com>
2026-04-05 20:43:54 +0300

    add push/pop methods tests

--- src/tests.cpp
+++ src/tests.cpp
@@ -4,13 +4,74 @@
 
 #include "../include/lock-free-queue.h"
 
-TEST_CASE("Test empty", "[empty]") {
+TEST_CASE("test empty method", "[empty]") {
     LockFreeQueue<int> q;
     REQUIRE(q.empty());
 
-    q.push(1);
+    for (int i = 0; i < 10; ++i) {
+        q.push(i);
+    }
     REQUIRE(!q.empty());
 
-    q.pop();
+    SECTION("push element") {
+        q.push(1);
+        REQUIRE(!q.empty());
+    }
+
+    SECTION("pop element") {
+        q.pop();
+        REQUIRE(!q.empty());
+    }
+
+    SECTION("pop all elements") {
+        for (int i = 0; i < 10; ++i) {
+            q.pop();
+        }
+        REQUIRE(q.empty());
+    }
+}
+
+TEST_CASE("test push method", "[push]") {
+    LockFreeQueue<int> q;
     REQUIRE(q.empty());
+
+    int push_element(10);
+    q.push(push_element);
+
+    REQUIRE(!q.empty());
+
+    auto pop_element = q.pop();
+    REQUIRE(q.empty());
+
+    REQUIRE(push_element == *pop_element);
+}
+
+TEST_CASE("test pop method", "[pop]") {
+    LockFreeQueue<int> q;
+    REQUIRE(q.empty());
+
+    SECTION("pop element") {
+        int push_element(10);
+        q.push(push_element);
+
+        REQUIRE(!q.empty());
+
+        auto pop_element = q.pop();
+        REQUIRE(q.empty());
+
+        REQUIRE(push_element == *pop_element);
+    }
+
+    SECTION("throw exception") {
+        std::shared_ptr<int> empty_ptr;
+        bool error = false;
+        try {
+            empty_ptr = q.pop();
+        } catch (const std::runtime_error& e) {
+            error = true;
+        }
+
+        REQUIRE(!empty_ptr);
+        REQUIRE(error);
+    }
 }
